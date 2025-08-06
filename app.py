13bc2814bea5c8c69f4cf191243abe3c8b24453a
BIgh06251 <bigh0625@gmail.com>
2025-08-07 00:17:21 +0200

    Update app.py

--- app.py
+++ app.py
@@ -32,4 +32,5 @@ def get_kilt_price():
         return jsonify({"error": "Failed to fetch price", "details": str(e)}), 500
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=5000)
+
