8144adb3db6a0a77f9627c41ae3d1631421e1191
BIgh06251 <bigh0625@gmail.com>
2025-08-07 00:33:27 +0200

    Update app.py

--- app.py
+++ app.py
@@ -31,6 +31,8 @@ def get_kilt_price():
     except Exception as e:
         return jsonify({"error": "Failed to fetch price", "details": str(e)}), 500
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+import os
 
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
