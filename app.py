dbf3d3b41a30feb72be70d650c010e1de8160792
BIgh06251 <bigh0625@gmail.com>
2025-08-07 00:35:29 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,12 @@
 from flask import Flask, jsonify
 import requests
+import os
 
 app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "RustSniper API is running!"
 
 @app.route("/snipes")
 def get_kilt_price():
@@ -31,8 +36,7 @@ def get_kilt_price():
     except Exception as e:
         return jsonify({"error": "Failed to fetch price", "details": str(e)}), 500
 
-import os
-
+# Bare kjør dette når du starter med `python app.py`, ikke med gunicorn
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
